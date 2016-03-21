cae900d6bc1549434d427897c6935f3dcff3cc54
humin <57751605@qq.com>
2016-03-21 15:09:57 +0800

    添加输入框下拉列表

--- client/scripts/App.js
+++ client/scripts/App.js
@@ -7,7 +7,7 @@ angular.module('app', dependencies);
 //路由模块
 angular.module('routeConfig', ['ngRoute']).config(function ($routeProvider) {
     var routes = [
-        'test', 'common/carousel', 'common/datePicker', 'common/dropDown', 'common/pagination'
+        'test', 'common/carousel', 'common/datePicker', 'common/dropDown', 'common/pagination', 'common/typeAhead'
     ];
     var setRoutes = function (route) {
         var url = '/' + route;

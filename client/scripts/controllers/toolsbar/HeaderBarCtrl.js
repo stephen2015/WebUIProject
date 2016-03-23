5b7535b4bf4e3bed44af679135f66403b45c611a
humin <57751605@qq.com>
2016-03-23 13:28:00 +0800

    更新天气预报外部接口访问服务

--- client/scripts/controllers/toolsbar/HeaderBarCtrl.js
+++ client/scripts/controllers/toolsbar/HeaderBarCtrl.js
@@ -2,31 +2,41 @@
  * Created by Stephen on 2016/3/22.
  */
 
-controllersModule.controller('HeaderBarCtrl', function ($scope, $timeout,$http, WeatherService) {
+controllersModule.controller('HeaderBarCtrl', function ($scope, $timeout, $http, WeatherService) {
     $scope.currTime = new Date();
-    $scope.name = 'World';
-    setInterval();
+    $scope.currWeather = {};
     getWeather();
+    setInterval();
+    setIntervalWeather();
 
     function setInterval() {
         return $timeout(function () {
             clock();
             return setInterval();
-        }, 1000);
+        }, 1000);//每秒更新一次
     };
 
+    function setIntervalWeather() {
+        return $timeout(function () {
+            getWeather();
+            return setIntervalWeather();
+        }, 1000*60*60*2);//每2小时更新一次
+    };
     function clock() {
         return $scope.currTime = new Date();
     };
 
     function getWeather() {
-        var district = '北京';
-        var format = 'json';
-        var authkey = 'c2336ed98ccb414fa438f29ce2ac61ef';
-        var callback = 'JSON_CALLBACK';
+        var district = '101190401'; //城市ID(苏州)
+        var format = 'json';        //返回格式
+        var authkey = 'c2336ed98ccb414fa438f29ce2ac61ef';//授权码
+        var callback = 'JSON_CALLBACK';//jsonp回调参数(必须的，不然报错)
         WeatherService.getTodayWeather(district, format, authkey, callback)
             .then(function (data) {
-                console.log(data.data);
+                if (data.status == 200) {
+                    console.log(data.data);
+                    $scope.currWeather = data.data;
+                }
             });
     }
 });

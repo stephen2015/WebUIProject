db17226aa2ada708de02fe540bb598d6fbe6c7c1
stephen <57751605@qq.com>
2016-03-24 14:20:25 +0800

    添加日历控件展示

--- client/scripts/controllers/personal/GridController.js
+++ client/scripts/controllers/personal/GridController.js
@@ -27,3 +27,32 @@ controllersModule.controller('GridCtrl', function ($scope, $http) {
     // 通过$watch currentPage和itemperPage 当他们一变化的时候，重新获取数据条目
     $scope.$watch('paginationConf.currentPage + paginationConf.itemsPerPage', reGetProducts);
 });
+
+/**
+ * 后台代码： php
+ * $product = new Product();
+
+ // 获取表中总行数
+ // 由于只要获取总行数，count(id)性能很好啊，千万不要用*哦，不然你就是作死
+ $total = $product->query('select count(id) as rows from product');
+ $total = $total[0]['rows'];
+
+ // 只需要获取当前页要展示的数据
+ $postData = json_decode(file_get_contents("php://input")); // 接收post过来的数据
+
+ $currentPage = $postData->currentPage; // 获取当前页
+ $perPage = $postData->itemsPerPage; // 获取每页多少条数据
+ $offset = ($currentPage - 1) * $perPage; // 计算偏移量
+
+ $sql = "select * from product limit $offset, $perPage";
+ $items = $product->query($sql);
+
+ // 组成产品信息数组
+ $productInfo = [
+ 'total' => $total,
+ 'items' => $items
+ ];
+
+ // 以json格式进行输出，方便js进行操作
+ echo json_encode($productInfo);
+ */

0745b6bd365a014b6bcc3df5dc2ae8f4dde39ef0
humin <57751605@qq.com>
2016-04-28 10:04:56 +0800

    更新爱心动画

--- client/scripts/controllers/common/TabsCtrl.js
+++ client/scripts/controllers/common/TabsCtrl.js
@@ -2,14 +2,14 @@
  * Created by Stephen on 2016/3/22.
  */
 
-controllersModule.controller('TabsCtrl', function ($scope, $window) {
+controllersModule.controller('TabsCtrl', function ($scope, $window, $timeout) {
     $scope.tabs = [
         { title:'Dynamic Title 1', content:'Dynamic content 1' },
         { title:'Dynamic Title 2', content:'Dynamic content 2', disabled: true }
     ];
 
     $scope.alertMe = function() {
-        setTimeout(function() {
+        $timeout(function() {
             $window.alert('You\'ve selected the alert tab!');
         });
     };
